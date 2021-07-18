60802430266fcb1d8d465bb65351e54aa10cd37c
anzar shaikh <anzarshaikh373@gmail.com>
2021-07-18 12:48:56 +0530

    Dats structure & algorithmaic programmes.done

---
+++ DataStructureD16-17/AlgorithmPrograms/com/b/programmes/Node.java
@@ -0,0 +1,14 @@
+package com.b.programmes;
+
+class Node {
+
+    public Node(Object data) {
+        super();
+        this.data = data;
+        this.next = null;
+    }
+
+
+    Object data;
+    Node next;
+}
---
+++ DataStructureD16-17/AlgorithmPrograms/com/b/programmes/PrimeNumber.java
@@ -0,0 +1,11 @@
+package com.b.programmes;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		Utility u = new Utility();
+		System.out.println("Enter the number:");
+	    int x = u.inputInteger();
+	    System.out.println(u.primeNumber(x));
+	}
+}
